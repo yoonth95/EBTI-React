944cc26d9d5f9003f49da6d6a76f86ee5a811245
윤태현 <yoonth0919@gmail.com>
2023-09-30 22:57:24 +0900

    View 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from 'components/Main';
 import Test from 'components/Test';
 import Result from 'components/Result';
+import View from 'components/View';
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
         <Route path='/' element={<Main />}/>
         <Route path='/test' element={<Test />}/>
         <Route path='/result' element={<Result />}/>
-        {/* <Route path='/view' element={<View />}/> */}
+        <Route path='/view' element={<View />}/>
       </Routes>
     </Router>
   )
