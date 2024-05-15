454770b158c08c0abe4e9611fab05b4c35b700d5
윤태현 <yoonth0919@gmail.com>
2024-05-15 17:37:34 +0900

    Add: insta 아이콘 추가

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { FooterBox, FooterHeader, FooterName, FooterIcons, FooterLink, Copyright } from 'styles/StyledComponents'
-import { faGithub, faVimeoV } from "@fortawesome/free-brands-svg-icons";
+import React from "react";
+import { FooterBox, FooterHeader, FooterName, FooterIcons, FooterLink, Copyright } from "styles/StyledComponents";
+import { faGithub, faInstagram, faVimeoV } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = ({ position, bottom }) => {
@@ -11,8 +11,15 @@ const Footer = ({ position, bottom }) => {
         <FooterName>yoonth0919</FooterName>
 
         <FooterIcons>
-          <FooterLink href='https://github.com/yoonth95' target='_blank'><FontAwesomeIcon icon={faGithub} /></FooterLink>
-          <FooterLink href='https://velog.io/@yoonth95' target='_blank'><FontAwesomeIcon icon={faVimeoV} /></FooterLink>
+          <FooterLink href="https://github.com/yoonth95" target="_blank">
+            <FontAwesomeIcon icon={faGithub} />
+          </FooterLink>
+          <FooterLink href="https://velog.io/@yoonth95" target="_blank">
+            <FontAwesomeIcon icon={faVimeoV} />
+          </FooterLink>
+          <FooterLink href="https://www.instagram.com/yoonth0919" target="_blank">
+            <FontAwesomeIcon icon={faInstagram} />
+          </FooterLink>
         </FooterIcons>
 
         <Copyright>Copyright © 2023 yoonth0919 All rights reserved.</Copyright>
